7afa36c47d67cc92476bd214516d5bd82644748d
anurag05072000 <121397208+anurag05072000@users.noreply.github.com>
2023-01-06 00:45:58 +0530

    Update BruteForcePrintAllPermutation.java

--- Recursion/BruteForcePrintAllPermutation.java
+++ Recursion/BruteForcePrintAllPermutation.java
@@ -1,31 +1,30 @@
 import java.util.*;  
 public class BruteForcePrintAllPermutation
 {
-	public static void recurPermute(int [] inp_arr,List<Integer> temp_ds,List<List<Integer>> out,boolean [] picked){
-
-    if(temp_ds.size()==inp_arr.length){
-      out.add(new ArrayList<>(temp_ds));
-      return;
+    public static void recurPermute(int [] inp_arr,List<Integer> temp_ds,List<List<Integer>> out,boolean [] picked){
+        if(temp_ds.size()==inp_arr.length){
+        out.add(new ArrayList<>(temp_ds));
+        return;
+        }
+        for(int i=0;i<inp_arr.length;i++){
+     	   if(!picked[i]){
+           temp_ds.add(inp_arr[i]);
+           picked[i]=true;
+           recurPermute(inp_arr,temp_ds,out,picked);
+           temp_ds.remove(Integer.valueOf(inp_arr[i]));
+           picked[i]=false;
+      	   }
+        }    
     }
-    for(int i=0;i<inp_arr.length;i++){
-     	if(!picked[i]){
-          temp_ds.add(inp_arr[i]);
-          picked[i]=true;
-          recurPermute(inp_arr,temp_ds,out,picked);
-          temp_ds.remove(Integer.valueOf(inp_arr[i]));
-          picked[i]=false;
-     	}
-      }    
-	}
-	public static List<List<Integer>> permute(int [] inp_arr){
+    public static List<List<Integer>> permute(int [] inp_arr){
 	List<List<Integer>> out = new ArrayList<>();
 	List<Integer> temp_ds = new ArrayList<>();
-     boolean [] picked = new boolean[inp_arr.length];
-     recurPermute(inp_arr,temp_ds,out,picked);
-     return out;
+        boolean [] picked = new boolean[inp_arr.length];
+        recurPermute(inp_arr,temp_ds,out,picked);
+        return out;
 	}
-	public static void main(String[] args){  
+     public static void main(String[] args){  
      int [] inp_arr = {1,2,3};
      System.out.println(permute(inp_arr));
-	}  
+     }  
 }
